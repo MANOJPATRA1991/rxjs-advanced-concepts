a5528483c2887710fdd8c11cd1e39c0d199093ff
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-02 13:01:39 +0530

    endWith/basic

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import { take, concatAll } from 'rxjs/operators';
-import { interval, of } from 'rxjs';
+import { endWith } from 'rxjs/operators';
+import { of } from 'rxjs';
 
-const obs1 = interval(1000).pipe(take(5));
-const obs2 = interval(500).pipe(take(2));
-const obs3 = interval(2000).pipe(take(1));
+const source$ = of('Hello', 'Friend', 'Goodbye');
 
-const source$ = of(obs1, obs2, obs3);
-
-const example$ = source$.pipe(concatAll());
-
-// output: 0,1,2,3,4,0,1,0
-const subscribe$ = example$.subscribe(val => console.log(val));
+/**
+ * Hello
+ * Friend
+ * Goodbye
+ * Friend
+ */
+source$.pipe(
+  endWith('Friend')
+).subscribe(console.log);
