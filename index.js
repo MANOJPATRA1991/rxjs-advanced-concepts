bf340f33dc916e4174d706043f0ebd59baac5745
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-02 05:52:39 +0530

    subscribe() with object map to handle error or completion

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ const myObservable = fromEvent(button, 'click');
 
 // For now, let's just log the event on each click
 const subscription = myObservable.subscribe(event => console.log(event));
+
+// Instead of a function, we will pass an object with next, error, and complete methods
+const newSubscription = myObservable.subscribe({
+  // On successful emissions
+  next: event => console.log(event),
+  // On errors
+  error: error => console.log(error),
+  // Called once on completion
+  complete: () => console.log('complete!')
+});
