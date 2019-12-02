47df203ff69d677a75a859048b69cc7ea4e23e33
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-02 10:42:47 +0530

    combineLatest with optional projection functions

--- index.js
+++ index.js
@@ -8,18 +8,14 @@ const timerTwo$ = timer(2000, 4000);
 const timerThree$ = timer(3000, 4000);
 
 // when one timer emits, emit the latest values from each timer as an array
-combineLatest(timerOne$, timerTwo$, timerThree$).subscribe(
-  ([timerValOne, timerValTwo, timerValThree]) => {
-    /*
-      Example:
-    timerOne first tick: 'Timer One Latest: 1, Timer Two Latest:0, Timer Three Latest: 0
-    timerTwo first tick: 'Timer One Latest: 1, Timer Two Latest:1, Timer Three Latest: 0
-    timerThree first tick: 'Timer One Latest: 1, Timer Two Latest:1, Timer Three Latest: 1
-  */
-    console.log(
-      `Timer One Latest: ${timerValOne},
-     Timer Two Latest: ${timerValTwo},
-     Timer Three Latest: ${timerValThree}`
-    );
+combineLatest(
+  timerOne$, 
+  timerTwo$, 
+  timerThree$,
+  // combineLatest also takes an optional projection function
+  (one, two, three) => {
+    return `Timer One (Proj) Latest: ${one}, 
+              Timer Two (Proj) Latest: ${two}, 
+              Timer Three (Proj) Latest: ${three}`;
   }
-);
+).subscribe(console.log);
